05444c2fd7ef29bdc3990630f67d4c235590659e
Alexander Peter <alexander.peter@bzz.ch>
2025-08-21 00:58:56 +0200

    implementation quit and help

--- src/main/java/ch/bzz/LibraryAppMain.java
+++ src/main/java/ch/bzz/LibraryAppMain.java
@@ -1,8 +1,22 @@
 package ch.bzz;
 
-public class LibraryAppMain {
+import java.util.Scanner;
 
+public class LibraryAppMain {
     public static void main(String[] args) {
-        System.out.println("HelloWorld");
+        Scanner sc = new Scanner(System.in);
+        boolean run = true;
+        while (run) {
+            String in = sc.nextLine();
+            if (in.equals("quit")) {
+                run = false;
+            } else if (in.equals("help")) {
+                System.out.println("help");
+                System.out.println("quit");
+            } else {
+                System.out.println(in);
+            }
+        }
+        sc.close();
     }
 }
