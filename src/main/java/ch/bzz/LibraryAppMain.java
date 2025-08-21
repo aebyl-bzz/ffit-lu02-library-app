bd2334238c4e578140f271e047218bd9fc60bc34
Alexander Peter <alexander.peter@bzz.ch>
2025-08-21 15:11:20 +0200

    book class and listBooks command

--- src/main/java/ch/bzz/LibraryAppMain.java
+++ src/main/java/ch/bzz/LibraryAppMain.java
@@ -3,6 +3,8 @@ package ch.bzz;
 import java.util.Scanner;
 
 public class LibraryAppMain {
+    private static final Book BOOK_1 = new Book(1, "978-3-8362-9544-4", "Java ist auch eine Insel", "Christian Ullenboom", 2023);
+    private static final Book BOOK_2 = new Book(2, "978-3-658-43573-8", "Grundkurs Java", "Dietmar Abts", 2024);
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         boolean run = true;
@@ -13,6 +15,10 @@ public class LibraryAppMain {
             } else if (in.equals("help")) {
                 System.out.println("help");
                 System.out.println("quit");
+                System.out.println("listBooks");
+            } else if (in.equals("listBooks")) {
+                System.out.println(BOOK_1.getTitle());
+                System.out.println(BOOK_2.getTitle());
             } else {
                 System.out.println(in);
             }
